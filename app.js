dc8116120192802b777090e1ef1fd199b0435306
andrei <git1@email.com>
2021-10-11 17:39:34 +0200

    added app server

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const courses = [
+    {
+        categoryId: "1",
+        categoryName: "Marketing",
+        totalCourses: "17",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "2",
+        categoryName: "UX Design",
+        totalCourses: "25",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "3",
+        categoryName: "Photography",
+        totalCourses: "13",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "4",
+        categoryName: "Business",
+        totalCourses: "20",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "5",
+        categoryName: "Marketing",
+        totalCourses: "17",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "6",
+        categoryName: "UX Design",
+        totalCourses: "25",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "7",
+        categoryName: "Photography",
+        totalCourses: "13",
+        image: "http://tny.im/q5J"
+    },
+    {
+        categoryId: "8",
+        categoryName: "Business",
+        totalCourses: "20",
+        image: "http://tny.im/q5J"
+    },
+]
+
+app.get('/courses', (req, res) => {
+    res.json(courses);
+})
+
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}`);
+})
